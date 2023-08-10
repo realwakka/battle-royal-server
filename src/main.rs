214e40ed504e8cea0fc5a74544213e2fc3301c6a
Sidong Yang <realwakka@gmail.com>
2023-08-10 11:02:46 +0900

    add front dist

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
 use warp::Filter;
+use futures_util::{FutureExt, StreamExt};
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let port : u16 = std::env::var("PORT").expect("no port env").parse().expect("failed to parse port");
 
-    warp::serve(hello)
-        .run(([0, 0, 0, 0], 10000))
+    let route = warp::path("/")
+	.and(warp::fs::dir("frontend/dist"));
+
+    // let hello = warp::path!("hello" / String)
+    //     .map(|name| format!("Hello, {}!", name));
+
+
+    // let routes = warp::path("socket")
+    //     .and(warp::ws())
+    //     .map(|ws: warp::ws::Ws| {
+    //         ws.on_upgrade(|websocket| {
+    //             // Just echo all messages back...
+    //             let (tx, rx) = websocket.split();
+    //             rx.forward(tx).map(|result| {
+    //                 if let Err(e) = result {
+    //                     eprintln!("websocket error: {:?}", e);
+    //                 }
+    //             })
+    //         })
+    //     });
+
+    warp::serve(route)
+        .run(([0, 0, 0, 0], port))
         .await;
 }
