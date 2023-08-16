087d3a156739b7c3f1673247bbd92208d2945980
Sidong Yang <realwakka@gmail.com>
2023-08-16 21:11:07 +0900

    update

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,48 +2,128 @@
 
 use std::f32::consts::PI;
 use bevy::prelude::*;
+use gloo_net::websocket::futures::WebSocket;
+// use tungstenite::stream::MaybeTlsStream;
+// use tungstenite::connect;
+// use std::net::TcpStream;
+use std::sync::{Arc, Mutex};
+use bevy::tasks::{TaskPool, Task};
+use futures::channel::mpsc::{Sender, Receiver, TryRecvError, TrySendError};
+use futures::StreamExt;
+use gloo_net::websocket::Message;
+use std::net::SocketAddr;
+use futures::SinkExt;
+use serde::{Deserialize, Serialize};
+
+#[derive(Resource)]
+pub struct Network {
+    send_channel: Sender<Message>,
+    position: Arc<Mutex<Vec2>>
+}
+
+#[derive(Serialize, Deserialize)]
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+#[derive(Serialize, Deserialize)]
+struct MoveRequest {
+    direction: Direction
+}
+
+impl Network {
+    pub fn new() -> Self {
+	let mut ws = WebSocket::open("ws://localhost:8080/socket").unwrap();
+	let (mut ws_write, mut ws_read) = ws.split();
+	
+	let (send, mut recv) = futures::channel::mpsc::channel::<Message>(100);
+	let res = wasm_bindgen_futures::spawn_local(async move {
+	    while let Some(msg) = recv.next().await {
+		info!("{:?}", msg);		
+		let _ = ws_write.send(msg.clone()).await;
+	    }
+	});
+
+	wasm_bindgen_futures::spawn_local(async move {
+	    while let Some(msg) = ws_read.next().await {
+		info!("from ws read {:?}", msg);		
+	    }
+	});	
+	
+        Self {
+	    send_channel: send,
+	    position: Arc::new(Mutex::new(Vec2::ZERO))
+        }
+    }
+
+    pub fn try_send(&mut self, message: Message) -> Result<(), TrySendError<Message>> {
+	self.send_channel.try_send(message)
+    }
+
+    pub fn parse_socket_addr(addr: &str) -> SocketAddr {
+        addr.parse().unwrap()
+    }
+}
 
 #[derive(Component)]
 struct Player {
     position: Vec2,
-    direction: Vec2,    
+    direction: Vec2,
 }
 
 #[derive(Component)]
 struct MyGameCamera;
 
 fn main() {
+    
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .add_systems(Update, (system, update_config, update_body))
         .run();
+
+}
+
+async fn ws_loop(ws: WebSocket, mut recv_channel: Receiver<Message>) {
+
+    while let Some(msg) = recv_channel.next().await {
+        info!("1. {:?}", msg);
+    }
+
+    // while Some(message) => recv_channel
+    // loop {
+    // 	futures::select! {
+    // 	    message = recv_channel.next() => {}
+    // 	};
+    // }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((Camera2dBundle::default(), MyGameCamera));
-    // // text
-    // commands.spawn(TextBundle::from_section(
-    //     "Hold 'Left' or 'Right' to change the line width",
-    //     TextStyle {
-    //         font: asset_server.load("fonts/FiraMono-Medium.ttf"),
-    //         font_size: 24.,
-    //         color: Color::WHITE,
-    //     },
-    // ));
-
     commands
 	.spawn(Player { position: Vec2::ZERO, direction: Vec2::ZERO })
 	.insert(TransformBundle::default());
     
+    commands.insert_resource(Network::new());
 }
 
-fn update_body(mut gizmos: Gizmos, mut query: Query<(&mut Player, &Transform)>) {
-    let (player, _transform) = query.single_mut();
+fn update_body(mut gizmos: Gizmos, mut query: Query<(&mut Player, &Transform)>, network: Res<Network>) {
+
+    // let pos = *network.into_inner().position.lock().unwrap();
+    let (mut player, _transform) = query.single_mut();
+    // gizmos.circle_2d(pos, 100., Color::PURPLE);
+
+    player.position = *network.into_inner().position.lock().unwrap();
+    
+}
+
+fn system(mut gizmos: Gizmos, mut query: Query<(&mut Player, &Transform)>, time: Res<Time>) {
+    let (player, _transform) = query.single_mut();    
     gizmos.circle_2d(player.position, 100., Color::PURPLE);
-}
-
-fn system(mut gizmos: Gizmos, time: Res<Time>) {
+    
     let sin = time.elapsed_seconds().sin() * 50.;
     gizmos.line_2d(Vec2::Y * -sin, Vec2::splat(-80.), Color::RED);
     gizmos.ray_2d(Vec2::Y * sin, Vec2::splat(80.), Color::GREEN);
@@ -69,21 +149,26 @@ fn system(mut gizmos: Gizmos, time: Res<Time>) {
 }
 
 fn update_config(mut config: ResMut<GizmoConfig>, keyboard: Res<Input<KeyCode>>, time: Res<Time>,
-		 mut query: Query<&mut Player>, mut q: Query<&mut Transform, With<MyGameCamera>>,) {
-
+		 mut query: Query<&mut Player>, mut q: Query<&mut Transform, With<MyGameCamera>>,
+		 mut network: ResMut<Network>,
+) {
     let mut player = query.single_mut();
-    if keyboard.pressed(KeyCode::Right) {
-	player.position.x += 1.0;
+    if keyboard.just_pressed(KeyCode::Right) {
+	player.position.x += 100.0;
+
+	let _ = network.try_send(Message::Text(String::from("asdf")));
     }
-    if keyboard.pressed(KeyCode::Left) {
-	player.position.x -= 1.0;
+    if keyboard.just_pressed(KeyCode::Left) {
+	player.position.x -= 100.0;
     }
 
-    if keyboard.pressed(KeyCode::Up) {
-	player.position.y += 1.0;
+    if keyboard.just_pressed(KeyCode::Up) {
+	player.position.y += 100.0;
     }
-    if keyboard.pressed(KeyCode::Down) {
-	player.position.y -= 1.0;
+    if keyboard.just_pressed(KeyCode::Down) {
+	player.position.y -= 100.0;
+	let result = network.try_send(Message::Text(String::from("test")));
+	info!("{:?}", result);
     }
 
     let mut t = q.single_mut();
